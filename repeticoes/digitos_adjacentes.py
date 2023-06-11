20ae7bacb96acd5d033a848388492dba690a0ee4
Joey Hetfield <joeydefenix@hotmail.com>
2023-06-11 12:38:15 -0300

    lint

--- repeticoes/digitos_adjacentes.py
+++ repeticoes/digitos_adjacentes.py
@@ -1,14 +1,16 @@
-#  Escreva um programa que receba um número inteiro na entrada e verifique se o número recebido possui ao menos um dígito com um dígito adjacente igual a ele. Caso exista, imprima "sim"; se não existir, imprima "não".
+#  Escreva um programa que receba um número inteiro na entrada e verifique se
+#  número recebido possui ao menos um dígito com um dígito adjacente igual
+# a ele. Caso exista, imprima "sim"; se não existir, imprima "não".
 
-n = input('Digite um número inteiro: ')
+n = input("Digite um número inteiro: ")
 adjacente = False
 
 for i in range(len(n) - 1):
     if n[i] == n[i + 1]:
         adjacente = True
         break
-    
+
 if adjacente:
-    print('sim')
+    print("sim")
 else:
-    print('não')
+    print("não")
--- repeticoes/primalidade.py
+++ repeticoes/primalidade.py
@@ -1,8 +1,8 @@
-n = int(input('Digite um número para verificar se ele é primo: '))
+n = int(input("Digite um número para verificar se ele é primo: "))
 
-for i in range(2, int(n ** 0.5) + 1):
+for i in range(2, int(n**0.5) + 1):
     if n % i == 0:
-        print(f'{n} não é primo')
+        print(f"{n} não é primo")
         break
 else:
-    print(f'{n} é primo')
+    print(f"{n} é primo")
